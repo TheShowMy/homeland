51a958e86b5043bd2773a0d6744fc0f20d2d8485
yinshuang.li <318817412@qq.com>
2022-03-15 18:28:37 +0800

    解决了回调函数里赋值外部访问的问题,可以正式开始写csv的功能了

--- assets/scripts/Manage/CsvManage.ts
+++ assets/scripts/Manage/CsvManage.ts
@@ -1,16 +1,19 @@
 
-import { _decorator, Component, Node, resources } from 'cc';
+import { _decorator, Component, Node, resources, color } from 'cc';
 const { ccclass, property } = _decorator;
 
- /**
-  * 配置csv文件读取并管理
-  */
+/**
+ * 配置csv文件读取并管理
+ */
 @ccclass('CsvManage')
 export class ScvManage {
+    private tableNames: string[] = [];
+    private tableDatas: string[] = [];
+
     private static instance: ScvManage;
     private constructor() {
         //初始化
-        this.loadCsvData();
+        this.loadCsvData(this.emm.bind(this));
     };
     static getInstance() {
         if (!this.instance) {
@@ -19,19 +22,14 @@ export class ScvManage {
         }
         return this.instance;
     }
-
-    /**
-     * 使用表名获取数据
-     */
-    private csvList = new Map();
     /**
      * 获取指定路径下的所有文件,返回文件列表
      * @param path 需要获取的路径
      * @returns 返回一个 string 数组
      */
-    getScvPath(path:string):string[]{
+    getScvPath(path: string): string[] {
         const infos = resources.getDirWithPath(path)
-        let paths = infos.map((info)=>{
+        let paths = infos.map((info) => {
             return info.path;
         })
         return paths;
@@ -40,43 +38,88 @@ export class ScvManage {
     /**
      * 将每个csv文件 通过[表名:数据]键值对的方法存储
      */
-    loadCsvData(){
-        const ScvPaths = this.getScvPath("csv")
+    private loadCsvData(cb: Function) {
+
+        //获取resources\csv目录下的所有文件
+        const ScvPaths = this.getScvPath("csv");
+        let a;
+        let b;
+        //感觉文件名获取文件里的文本数据
         for (const ScvPath of ScvPaths) {
-            resources.load(ScvPath,(err,ScvData)=>{
+            const e = resources.load(ScvPath, (err, ScvData) => {
                 if (err) {
-                    console.log("读取csv文件报错:"+ err);
+                    console.log("读取csv文件报错:" + err);
                     return;
                 }
-                console.log(ScvPath.split("/",2)[1]);
-                this.csvList.set(ScvPath.split("/",2)[1],ScvData["text"]);
-            })
+
+                //将表名和数据分别存放到两个数值中
+                a = ScvPath.split("/", 2)[1];
+                b = ScvData["text"];
+                cb(a, b);
+            });
         }
     }
-    /**
-     * 通过csv文件名 获取其中的数据
-     * @param name csv文件名
-     * @returns 
-     */
-    getDataByCsvName(name:string){
-        const data:string =  this.csvList[0].get(name);
-        console.log(data);
-        
-        let dataArr = new Array<string[]>();
-        for (let emm of data.split("\n")) {
 
-            //dataArr.push(emm.split(","));
-            console.log(emm);
-            
+    emm(tableName:string, tableData:string) {
+        this.tableNames.push(tableName)
+        this.tableDatas.push(tableData)
+        console.log(this.tableDatas);
+        console.log(this.tableDatas[0]);
+    }
+
+    /**
+     * 通过表名获取指定配置表数据
+     * @param tableName 表名
+     * @returns 返回表里的所有数据,Strin类型,不存在该表返回null
+     */
+    public getCsvDataAll(tableName: string): string {
+
+        for (let i = 0; i < this.tableNames.length; i++) {
+            if (this.tableNames[i] === tableName) {
+                return this.tableDatas[i];
+            } else {
+                return null;
+            }
         }
-        // dataArr.join();
-        // console.log(dataArr);
-        
-        // if (dataArr) {
-        //     return dataArr
-        // }else{
-        //     return null
-        // }
+
+    }
+    /**
+     * 获取指定配置表的指定行
+     * @param tableName 表名
+     * @param list 第几行 1-max
+     * @returns 返回指定行的数据:strin 表名或者行数不存在时 返回null
+     */
+    public getCsvDataList(tableName: string, list: number) {
+        const data: string = this.getCsvDataAll(tableName);
+        if (data != null && list != 0) {
+            let dataLists = data.split("\n");
+            if (dataLists.length < list) {
+                return null;
+            }
+            return dataLists[list - 1];
+
+        } else {
+            return null;
+        }
+
+    }
+    public getCsvDataRow(tableName: string, row: number) {
+        const data: string = this.getCsvDataAll(tableName);
+        if (data != null && row != 0) {
+            let dataLists = data.split("\n");
+            if (dataLists[0].split(",").length < row) {
+                return null;
+            }
+
+        } else {
+            return null;
+        }
+    }
+    public inquireCsvDataList(tableName: string, row: number, rowValue: string) {
+
+    }
+    public inquireCsvDataListRow(tableName: string, row: number, rowValue: string, whichColumn: number) {
+
     }
 
 }
