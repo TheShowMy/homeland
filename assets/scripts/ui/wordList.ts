084a16cd7012406a435708baf4c16af5fc9c16c9
TheShow <318817412@qq.com>
2022-03-20 18:51:30 +0800

    master

---
+++ assets/scripts/ui/wordList.ts
@@ -0,0 +1,21 @@
+
+import { _decorator, Component, Node, Label } from 'cc';
+import { ComponentBase } from '../Manage/ComponentBase';
+const { ccclass, property } = _decorator;
+
+
+ 
+@ccclass('wordList')
+export class wordList extends ComponentBase {
+
+    @property(Label)
+    public lable:Label = null;
+
+    public  (name:string){
+        this.lable.string = name;
+    }
+
+
+}
+
+
